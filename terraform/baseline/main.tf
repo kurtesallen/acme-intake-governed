013044e56ba17b41360392bee06d9962aee87f59
Kurtes <kurtesallen@gmail.com>
2026-05-27 23:51:39 -0400

    Add CloudTrail bucket policy and finalize baseline

--- terraform/baseline/main.tf
+++ terraform/baseline/main.tf
@@ -46,6 +46,44 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
 }
 
 # -----------------------------
+# CloudTrail Bucket Policy (Required)
+# -----------------------------
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket_policy" "cloudtrail_logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.logs.id}"
+      },
+      {
+        Sid    = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.logs.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# -----------------------------
 # CloudTrail (AU-2)
 # -----------------------------
 resource "aws_cloudtrail" "baseline" {
